0c8f02a22517a6e08178fedc1543dbeff3ce1182
geomer198 <geomer198@gmail.com>
2022-10-07 23:55:33 +0300

    [FIX] Removed old module.

--- purchase_vendor_bill_product_breakdown/models/res_partner.py
+++
@@ -1,7 +0,0 @@
-from odoo import fields, models
-
-
-class ResPartner(models.Model):
-    _inherit = "res.partner"
-
-    bill_components = fields.Boolean(string="Vendor Bill Breakdown")
--- purchase_vendor_bill_product_breakdown/tests/__init__.py
+++
@@ -1,5 +0,0 @@
-from . import test_supplierinfo
-from . import test_account_move_create
-from . import test_purchase_order
-from . import test_purchase_order_line
-from . import test_components_flow
